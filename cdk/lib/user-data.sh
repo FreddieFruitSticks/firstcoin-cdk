c41997116f0b5640c968daaef222d17e6ed19491
Freddie <freddieodonnell@gmail.com>
2022-10-04 16:13:53 +0200

    update user-data to install docker

--- cdk/lib/user-data.sh
+++ cdk/lib/user-data.sh
@@ -23,12 +23,13 @@ TEST_FILE=/home/ubuntu/user-data-has-run-once.test
 
 #This is just to run once since the above cloud_final_modules setting is set to run on every reboot 
 #An alternative is to run a systemd script that runs on every reboot to d/l a script from s3 and run that
-if [-f "$TEST_FILE"]
-then
-
+if [ -f "$TEST_FILE" ]; then
+    echo "$TEST_FILE exists. VM Initialised."
 else
-    sudo su
+    echo "$TEST_FILE does not exist. Initialising VM."
     apt update -y
+
+    echo "Installing nginx..."
 
     apt install -y nginx
     systemctl start nginx
@@ -37,9 +38,19 @@ else
     chmod 2775 /usr/share/nginx/html
     find /usr/share/nginx/html -type d -exec chmod 2775 {} \;
     find /usr/share/nginx/html -type f -exec chmod 0664 {} \;
+    echo '<h1>It worked!</h1>' > /var/www/html/index.nginx-debian.html
+
+    echo "nginx Installed"
+
+    echo "installing docker ..."
+    
+    apt-get install ca-certificates curl gnupg lsb-release
+    mkdir -p /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    apt-get update
+    apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+    service docker start
 
     touch $TEST_FILE
 fi
-
-
-echo '<h1>It worked!</h1>' > /var/www/html/index.nginx-debian.html
